93f664a3b96999b3912fe3e841e4183e4f4b3b1a
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-05-13 16:54:11 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -22,6 +22,16 @@ ENVIRONMENT () {
 	apt install python-pip -y > /dev/null 2>&1;
 	apt install snapd -y > /dev/null 2>&1;
 	apt install python3-pip -y > /dev/null 2>&1;
+	sudo apt install snapd
+	sudo snap install amass
+	sudo apt-get install parallel
+	sudo rm /etc/parallel/config
+	sudo apt-get --assume-yes install git make gcc
+	sudo apt-get install -y grepcidr
+	sudo apt install wkhtmltopdf
+	apt install -y libpcap-dev
+	pip install gdown
+	sudo ln -s ~/.local/bin/gdown /usr/bin/gdown
 }
 
 DNS_RESOLVER () {
@@ -33,6 +43,119 @@ DNS_RESOLVER () {
 	echo -e ${BLUE}"[DNS RESOLVER]" ${RED}"PureDNS installation in progress ...";
 	GO111MODULE=on go install github.com/d3mondev/puredns/v2@latest > /dev/null 2>&1 && ln -s ~/go/bin/puredns /usr/local/bin;
 	echo -e ${BLUE}"[DNS RESOLVER]" ${GREEN}"PureDNS installation is done !"; echo "";
+	
+	git clone https://github.com/robertdavidgraham/masscan
+	cd masscan
+	make
+	make install
+	
+	go install github.com/lc/subjs@latest
+	cp $HOME/go/bin/subjs /usr/local/bin
+
+	go install github.com/lc/gau/v2/cmd/gau@latest
+	cp $HOME/go/bin/gau /usr/local/bin
+
+	go install github.com/hakluke/haklistgen@latest
+	cp $HOME/go/bin/haklistgen /usr/local/bin
+	pip3 install dnsgen
+	
+	go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+	cp $HOME/go/bin/nuclei /usr/local/bin
+	
+	go install github.com/tomnomnom/hacks/tok@master
+	cp $HOME/go/bin/tok /usr/local/bin
+	
+	go install github.com/tomnomnom/assetfinder@latest
+	cp $HOME/go/bin/assetfinder /usr/local/bin
+	
+	go install github.com/tomnomnom/httprobe@master
+	cp $HOME/go/bin/httprobe /usr/bin/
+	
+	go install github.com/haccer/subjack@latest
+	cp $HOME/go/bin/subjack /usr/bin/
+	
+	go install github.com/sensepost/gowitness@latest
+	cp $HOME/go/bin/gowitness /usr/local/bin
+	
+	go install github.com/hakluke/hakrawler@latest
+	cp $HOME/go/bin/hakrawler /usr/local/bin
+
+	go install github.com/Josue87/gotator@latest
+	cp $HOME/go/bin/gotator /usr/local/bin
+	
+	go install -v github.com/tomnomnom/anew@latest
+	cp $HOME/go/bin/anew /usr/local/bin
+	
+	go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+	cp $HOME/go/bin/httpx /usr/local/bin
+
+	go install github.com/jaeles-project/gospider@latest
+	cp $HOME/go/bin/gospider /usr/local/bin
+
+	go install github.com/tomnomnom/unfurl@latest
+	cp $HOME/go/bin/unfurl /usr/local/bin
+	
+	go install github.com/OJ/gobuster/v3@latest
+	cp $HOME/go/bin/gobuster /usr/local/bin
+
+	go install github.com/ffuf/ffuf/v2@latest
+	cp $HOME/go/bin/ffuf /usr/local/bin
+
+	go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+	cp $HOME/go/bin/subfinder /usr/local/bin
+	
+	go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+	cp $HOME/go/bin/naabu /usr/local/bin
+	
+	go install -v github.com/projectdiscovery/wappalyzergo/cmd/update-fingerprints@latest
+	cp $HOME/go/bin/wappalyzergo /usr/local/bin
+	
+	cd $HOME/BB
+	git clone https://github.com/vortexau/dnsvalidator.git
+	cd dnsvalidator
+	pip install -r requirements.txt
+	python3 setup.py install
+	
+	cd $HOME
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	source $HOME/.cargo/env
+	rustup update
+	sudo apt install cargo
+	
+	cd $HOME
+	git clone https://github.com/findomain/findomain.git
+	cd findomain
+	cargo build --release
+	sudo cp target/release/findomain /usr/bin/
+	
+	cd $HOME/BB
+	git clone https://github.com/Josue87/AnalyticsRelationships.git
+	cd AnalyticsRelationships/
+	go build -ldflags "-s -w"
+	
+	cd $HOME/BB
+	git clone https://github.com/SpiderLabs/HostHunter.git
+	cd HostHunter
+	pip3 install -r requirements.txt
+
+	cd $HOME
+	cargo install ripgen
+	
+	cd $HOME/BB
+	
+	git clone https://github.com/shmilylty/OneForAll.git
+	cd OneForAll/
+	python3 -m pip install -U pip setuptools wheel
+	pip3 install -r requirements.txt
+	
+	cd $HOME/BB
+	git clone https://github.com/dariusztytko/vhosts-sieve.git
+	pip3 install -r vhosts-sieve/requirements.txt
+	
+	cd $HOME/BB
+	git clone https://github.com/punk-security/dnsReaper.git
+	cd dnsReaper
+	pip install -r requirements.txt
 }
 
 ENVIRONMENT && DNS_RESOLVER;
