fa958068af39d87043128bba2c85e6b3555d8207
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-05-21 00:43:04 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -186,6 +186,8 @@ DNS_RESOLVER () {
 	cd xnLinkFinder
 	sudo python3 setup.py install
 	
+	sudo rm -rf /usr/local/go 
+	
 	sudo wget https://raw.githubusercontent.com/Azathothas/BugGPT-Tools/main/linky/linky.sh -O /usr/local/bin/linky && sudo chmod +xwr /usr/local/bin/linky && linky --help
 	
 	cd $HOME
