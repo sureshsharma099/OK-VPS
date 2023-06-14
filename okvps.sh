f67c8e0e7493492dcd6d053b6460b9ada21295fd
Nishant Bhagat <nishantbhagat57@gmail.com>
2023-06-14 16:36:47 +0530

    Update okvps.sh

--- okvps.sh
+++ okvps.sh
@@ -219,6 +219,15 @@ DNS_RESOLVER () {
 	wget https://github.com/Nishantbhagat57/My-Bug-Hunting-Repo/raw/main/swagger.yaml
 	
 	cd $HOME/BB
+	wget https://github.com/Nishantbhagat57/My-Bug-Hunting-Repo/raw/main/sqli-symfony.yaml
+	
+	cd $HOME/BB
+	wget https://github.com/Nishantbhagat57/My-Bug-Hunting-Repo/raw/main/symfony-profiler.yaml
+	
+	cd $HOME/BB
+	wget https://github.com/Nishantbhagat57/My-Bug-Hunting-Repo/raw/main/CVE-2015-4050.yaml
+	
+	cd $HOME/BB
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/$USER/.bashrc
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
